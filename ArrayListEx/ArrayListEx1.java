a77529e3cb20bd2602dbd73ca53b6aee88678e65
Vlad Kalugin <vladisloveworks@gmail.com>
2024-03-17 22:26:03 +0300

    Second Form

--- ArrayListEx/ArrayListEx1.java
+++ ArrayListEx/ArrayListEx1.java
@@ -7,10 +7,10 @@ public class ArrayListEx1 {
     public static void main(String[] args) {
 
         List<String> list1 = new ArrayList<>();
-        list1.add(0, "Влад");
-        list1.add(1, "Анна");
-        list1.add(2, "Игорь");
-        list1.add(3, "Глеб");
+        list1.add(0, "Vlad");
+        list1.add(1, "Anna");
+        list1.add(2, "Igor");
+        list1.add(3, "Gleb");
 
         for (String s : list1) {
             System.out.println(s);
