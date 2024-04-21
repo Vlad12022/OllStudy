63133fa74933e972b0c8927145b581cc4f05b64b
Vlad Kalugin <vladisloveworks@gmail.com>
2024-04-21 14:24:48 +0300

    Added new class, interface,another version Function interface

--- GitHub/Main.java
+++ GitHub/Main.java
@@ -1,11 +1,17 @@
 package GitHub;
 
+import java.util.function.BinaryOperator;
+
 public class Main {
     public static void main(String[] args) {
-        java.util.function.BiFunction<Double, Double, Double> radius =
-                (a, b) -> (a + b - Math.sqrt(a*a + b*b)) / 2;
 
-        System.out.println("Радиус вписанной окружности: " + radius.apply(3.0, 4.0));
+        BinaryOperator<Double> radius = (a, b) -> {
+            double c = Math.sqrt(a * a + b * b);
+            return (a + b - c) / 2;
+        };
+        System.out.println("Радиус вписанной окружности равен : " + radius.apply(20.0, 14.0));
+
+
     }
 }
 
